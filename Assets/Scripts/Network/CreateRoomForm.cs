a7c1ef6ae994de9cd913e160531a3f5b7ea11588
Daryoush Farsimadan <farsimadan.daryoush@gmail.com>
2020-05-31 18:22:15 +0300

    Tweak UI + Add Rainbow Folders

--- Assets/Scripts/Network/CreateRoomForm.cs
+++ Assets/Scripts/Network/CreateRoomForm.cs
@@ -2,21 +2,31 @@
 using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
+using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using WebSocketSharp;
 
 public class CreateRoomForm : MonoBehaviourPunCallbacks
 {
-    [SerializeField] private InputField _name;
+    [SerializeField] private TMP_InputField _name;
+    [SerializeField] private Slider _maxPlayers;
 
     public void SubmitForm()
     {
-        PhotonNetwork.CreateRoom(_name.text);
+        RoomOptions options = new RoomOptions();
+        byte maxplayers = (byte)Convert.ToInt32(_maxPlayers.value);
+        Debug.Log(maxplayers);
+        options.MaxPlayers = maxplayers;
+        PhotonNetwork.CreateRoom(_name.text, options);
         
     }
 
     public override void OnCreatedRoom()
     {
+     
+
             if (PhotonNetwork.InLobby)
             {
                 PhotonNetwork.LeaveLobby();
